8bc56e6076cf12e7c721387ecabd2ace0dce00f6
Teguhriyadi <ilham.teguh55@gmail.com>
2022-12-02 17:15:53 +0700

    Update Migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -7,16 +7,20 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+    * Run the migrations.
+    *
+    * @return void
+    */
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-<<<<<<< HEAD
+            $table->date('tglbim')->nullable();
             $table->string('name');
+            $table->string('kelas')->nullable();
+            $table->text('bimbingan')->nullable();
+            $table->text('tanggapan')->nullable();
+            $table->text('keterangan')->nullable();
             $table->string('level');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
@@ -24,28 +28,13 @@ return new class extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
-=======
-            $table->date('tglbim')->nullable();
-             $table->string('name');
-             $table->string('kelas')->nullable();
-             $table->text('bimbingan')->nullable();
-             $table->text('tanggapan')->nullable();
-             $table->text('keterangan')->nullable();
-             $table->string('level');
-             $table->string('email')->unique();
-             $table->timestamp('email_verified_at')->nullable();
-             $table->string('password');
-             $table->rememberToken();
-             $table->timestamps();
-         });
->>>>>>> df2a529e8ba7cc112d0aa55b865966d5c9b8b95c
     }
 
     /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
+    * Reverse the migrations.
+    *
+    * @return void
+    */
     public function down()
     {
         Schema::dropIfExists('users');
